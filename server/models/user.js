d3f78ac87a4752bf2bb92f8dbbd910cb3c0f15fa
Michael <13236924+Mirobit@users.noreply.github.com>
2020-09-14 15:16:17 +0200

    Add user model

---
+++ server/models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["user", "fdz", "admin"],
+    default: "user",
+  },
+  userStatus: {
+    type: String,
+    enum: [
+      "new",
+      "awatingVerification",
+      "verified",
+      "awaitingApproval",
+      "approved",
+    ],
+    default: "new",
+  },
+  institution: {
+    type: Schema.Types.ObjectId,
+    ref: "Institution",
+  },
+  applications: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Applications",
+    },
+  ],
+});
+
+module.exports = mongoose.model("User", userSchema);
