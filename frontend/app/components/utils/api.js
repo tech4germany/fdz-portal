7417e2fea35af8bc0cc0a09f320e1933f0f18288
Michael <13236924+Mirobit@users.noreply.github.com>
2020-09-21 18:10:55 +0200

    Add frontend api service

---
+++ frontend/app/components/utils/api.js
@@ -0,0 +1,51 @@
+const BASE_URL = window.location.origin;
+
+const sendData = async (endpoint, type, data) => {
+  const url = BASE_URL + "/api" + endpoint;
+  const options = {
+    method: type,
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${localStorage.getItem("identity")}`,
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+    body: JSON.stringify(data),
+  };
+  const response = await fetch(url, options);
+  const result = await response.json();
+  handleError(result);
+  return result;
+};
+
+const getData = async (endpoint) => {
+  const url = BASE_URL + "/api" + endpoint;
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("identity")}`,
+    },
+  };
+
+  const response = await fetch(url, options);
+  const result = await response.json();
+  handleError(result);
+  return result;
+};
+
+const handleError = (result) => {
+  if (result.status === 200) return;
+
+  if (result.status === 401) {
+    window.location.pathname = "/login";
+    return;
+  }
+
+  displayMessage(
+    false,
+    result.status === 400
+      ? result.message
+      : "Unexpected server error. Couldn't finish action."
+  );
+};
+
+export { sendData, getData };
