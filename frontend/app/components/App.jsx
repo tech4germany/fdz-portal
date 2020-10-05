6e144986e27d1a8c224018fccc9a2bebe9746e02
Michael <13236924+Mirobit@users.noreply.github.com>
2020-10-05 15:56:25 +0200

    Add validation for expired tokens

--- frontend/app/components/App.jsx
+++ frontend/app/components/App.jsx
@@ -25,18 +25,27 @@ export default class App extends React.Component {
     };
 
     this.setUser = this.setUser.bind(this);
+    this.logout = this.logout.bind(this);
   }
 
   setUser(init) {
     const token = localStorage.getItem("identity");
     if (token) {
       const decoded = jwtDecode(token);
-      delete decoded.iat;
+      if (decoded.exp < Math.round(Date.now() / 1000)) {
+        return null;
+      }
+
       if (init) return decoded;
       this.setState({ user: decoded });
     } else {
       return null;
     }
+  }
+
+  logout() {
+    this.setState({ user: null });
+    localStorage.removeItem("identity");
   }
 
   render() {
